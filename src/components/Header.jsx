bd3a2a066f52f34484a5cbdec602908570cd0119
KingsDemise <bttb.kingshukk@gmail.com>
2023-08-21 15:25:44 +0530

    added comments functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,30 @@ import { PlusCircleIcon, HomeIcon } from "@heroicons/react/outline";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { modalState } from "../../atom/modalAtom";
 import { useRecoilState } from "recoil";
+import { useRouter } from "next/router";
 const Header = () => {
   const { data: session } = useSession();
   const [open, setOpen] = useRecoilState(modalState);
+  const router = useRouter();
   return (
     <div className="shadow-sm border-b sticky top-0 bg-gradient-to-tr from-red-200 to-yellow-100">
       <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-        <div className=" hidden  p-5 cursor-pointer relative lg:inline-grid bg-gradient-to-r from-black to-cyan-600 bg-clip-text text-transparent max-sm:hidden text-3xl font-satoshi font-bold italic tracking-wide">
+        <div
+          onClick={() => router.push("/")}
+          className=" hidden  p-5 cursor-pointer relative lg:inline-grid bg-gradient-to-r from-black to-cyan-600 bg-clip-text text-transparent max-sm:hidden text-3xl font-satoshi font-bold italic tracking-wide"
+        >
           Moments
         </div>
-        <div className="p-5  bg-gradient-to-r from-black to-cyan-600 bg-clip-text text-transparent max-sm:hidden text-3xl font-satoshi font-bold italic tracking-wide cursor-pointer relative  lg:hidden">
+        <div
+          onClick={() => router.push("/")}
+          className="p-5  bg-gradient-to-r from-black to-cyan-600 bg-clip-text text-transparent max-sm:hidden text-3xl font-satoshi font-bold italic tracking-wide cursor-pointer relative  lg:hidden"
+        >
           M
         </div>
 
         <div className="flex space-x-4 items-center">
           <HomeIcon
-            // onClick={() => router.push("/")}
+            onClick={() => router.push("/")}
             className="text-black hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"
           />
           <>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,49 @@
 "use client";
 import { ArrowUpIcon, ChatIcon } from "@heroicons/react/outline";
-import React, { useRef, useState } from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+import { useSession } from "next-auth/react";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import Moment from "react-moment";
 export default function Post({ img, userImg, caption, username, id }) {
-  const [comment, setComment] = useState("");
   const commentInputRef = useRef(null);
   const handleChatIconClick = () => {
     if (commentInputRef.current) {
       commentInputRef.current.focus();
     }
   };
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  async function sendComment(event) {
+    event.preventDefault();
+    const commentToSend = comment;
+    setComment("");
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.name,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  }
+  const { data: session } = useSession();
+  useEffect(() => {
+    const fetchComments = onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db, id]);
   return (
     <div className="bg-white my-7 border rounded-md">
       <div className="flex items-center p-5">
@@ -21,37 +55,61 @@ export default function Post({ img, userImg, caption, username, id }) {
         <p className="flex-1 text-black">{username}</p>
       </div>
       <img className="object-cover border p-4 w-full " src={img} alt="" />
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
-          <ArrowUpIcon
-            // onClick={likePost}
-            className="text-black btn"
-          />
-          <ChatIcon className="btn text-black " onClick={handleChatIconClick} />
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <ArrowUpIcon className="btn text-black" />
+            <ChatIcon className="btn text-black" />
+          </div>
         </div>
-      </div>
+      )}
       <p className="p-5 truncate text-black">
         <span className="font-bold mr-2 ">{caption}</span>
       </p>
-      <form className="flex items-center p-4">
-        <img className="h-8 w-8 rounded-full" src={userImg} alt={username} />
-        <input
-          value={comment}
-          ref={commentInputRef}
-          onChange={(event) => setComment(event.target.value)}
-          className=" border-none flex-1 focus:ring-0 text-black"
-          type="text"
-          placeholder="Share your thoughts..."
-        />
-        <button
-          type="submit"
-          // onClick={sendComment}
-          // disabled={!comment.trim()}
-          className="text-blue-400 font-bold disabled:text-blue-200"
-        >
-          Send
-        </button>
-      </form>
+      {comments.length > 0 && (
+        <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
+          {comments.map((comment, index) => (
+            <div
+              key={index}
+              className="flex items-center space-x-2 mb-2 text-black"
+            >
+              <img
+                className="h-7  rounded-full object-cover"
+                src={comment.data().userImage}
+                alt="user-image"
+              />
+              <p className="font-semibold text-black">
+                {comment.data().username}
+              </p>
+              <p className="flex-1 truncate text-black">
+                {comment.data().comment}
+              </p>
+              <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+            </div>
+          ))}
+        </div>
+      )}
+      {session && (
+        <form className="flex items-center p-4">
+          <img className="h-8 w-8 rounded-full" src={userImg} alt={username} />
+          <input
+            value={comment}
+            ref={commentInputRef}
+            onChange={(event) => setComment(event.target.value)}
+            className=" border-none flex-1 focus:ring-0 text-black"
+            type="text"
+            placeholder="Share your thoughts..."
+          />
+          <button
+            type="submit"
+            onClick={sendComment}
+            disabled={!comment.trim()}
+            className="text-blue-400 font-bold disabled:text-blue-200"
+          >
+            Send
+          </button>
+        </form>
+      )}
     </div>
   );
 }
